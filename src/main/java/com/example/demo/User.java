d11bccee2fc717cbbf89e95af87bb4172bc936f4
zp <27754469@qq.com>
2020-05-19 16:34:26 +0800

    删除两个属性

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -5,8 +5,6 @@ public class User {
     private String email;
     private String name;
     private String application;
-    private Integer age;
-    private String sex;
 
     //新增一个set方法
     public void setId(Integer id) {
